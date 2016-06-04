f43c03c177ec60b15a48764f0d4a1f613baf4467
Aaron Chang <aaron.chang@vpon.com>
2016-06-04 10:21:19 +0800

    for swiper, set key bindings after swiper is loaded

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -8,26 +8,40 @@
 (global-set-key (kbd "C-:") 'avy-goto-char)
 (global-set-key (kbd "C-x C-;") 'avy-goto-line)
 
-;;; projectile
-(setq projectile-keymap-prefix (kbd "C-c C-p"))
-(projectile-global-mode)
-(setq projectile-completion-system 'ivy)
-
 ;;; swiper
-(ivy-mode 1)
-(require 'swiper)
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
-(setq ivy-display-style 'fancy)
-(setq enable-recursive-minibuffers t)
-(global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "C-c r") 'ivy-resume)
 (global-set-key (kbd "C-c b") 'ivy-switch-buffer)
 (global-set-key (kbd "C-c k") 'counsel-ag)
 (global-set-key (kbd "C-x C-m") 'counsel-M-x)
-(define-key swiper-map (kbd "C-.")
-  (lambda () (interactive) (insert (format "\\<%s\\>" (with-ivy-window (thing-at-point 'symbol))))))
-(define-key swiper-map (kbd "M-.")
-  (lambda () (interactive) (insert (format "\\<%s\\>" (with-ivy-window (thing-at-point 'word))))))
+(global-set-key (kbd "C-s") 'swiper)
+(eval-after-load 'swiper
+  '(progn
+     (define-key swiper-map (kbd "C-.")
+       (lambda ()
+         (interactive)
+         (insert
+          (format
+           "\\<%s\\>"
+           (with-ivy-window
+             (thing-at-point 'symbol))))))
+     (define-key swiper-map (kbd "M-.")
+       (lambda ()
+         (interactive)
+         (insert
+          (format
+           "\\<%s\\>"
+           (with-ivy-window
+             (thing-at-point 'word))))))
+     (setq ivy-height 10)
+     (setq ivy-use-virtual-buffers t)
+     (setq ivy-count-format "(%d/%d) ")
+     (setq ivy-display-style 'fancy)
+     (setq enable-recursive-minibuffers t)
+     ))
+
+;;; projectile
+(setq projectile-keymap-prefix (kbd "C-c C-p"))
+(projectile-global-mode)
+(setq projectile-completion-system 'ivy)
 
 (provide 'init-local)
