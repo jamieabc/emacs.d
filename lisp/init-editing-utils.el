94c62009f63c443644843b275bbc958967afc6af
Aaron Chang <aaron.chang@vpon.com>
2017-06-07 08:26:38 +0800

    remove comment line

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -89,18 +89,6 @@
 (diminish 'undo-tree-mode)
 
 
-;; (require-package 'highlight-symbol)
-;; (dolist (hook '(prog-mode-hook html-mode-hook css-mode-hook))
-;;   (add-hook hook 'highlight-symbol-mode)
-;;   (add-hook hook 'highlight-symbol-nav-mode))
-;; (add-hook 'org-mode-hook 'highlight-symbol-nav-mode)
-;; (after-load 'highlight-symbol
-;;   (diminish 'highlight-symbol-mode)
-;;   (defadvice highlight-symbol-temp-highlight (around sanityinc/maybe-suppress activate)
-;;     "Suppress symbol highlighting while isearching."
-;;     (unless (or isearch-mode
-;;                 (and (boundp 'multiple-cursors-mode) multiple-cursors-mode))
-;;       ad-do-it)))
 (when (maybe-require-package 'symbol-overlay)
   (dolist (hook '(prog-mode-hook html-mode-hook css-mode-hook))
     (add-hook hook 'symbol-overlay-mode))
