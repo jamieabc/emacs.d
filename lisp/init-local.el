3970dc9753192dd43c2ef5cc3902786409bb038f
Aaron Chang <aaron.chang@vpon.com>
2017-06-17 09:30:04 +0800

    remove typescript settings

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -609,34 +609,8 @@
 ;; (editorconfig-mode 1)
 ;;; editorconfig
 
-;;; typescript
-(require-package 'tide)
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (company-mode +1))
-
 ;; ;; ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
-
-;; ;; ;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-;; ;; ;;; ts
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . js2-mode))
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (when (string-equal "ts" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
-
-;; ;;; js
-;; (add-hook 'js2-mode-hook #'setup-tide-mode)
-;;; typescript
 
 ;;; hackernews
 (require-package 'hackernews)
