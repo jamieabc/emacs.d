4e9b8b0d14b28b3c911ce67b47decff12a817aee
Aaron Chang <aaron.chang@quadas.com>
2018-05-25 18:49:55 +0800

    update theme settings

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,3 +1,30 @@
+;;; theme
+(require-package 'solarized-theme)
+
+;; make the fringe stand out from the background
+(setq solarized-distinct-fringe-background t)
+
+;; Don't change the font for some headings and titles
+(setq solarized-use-variable-pitch nil)
+
+;; make the modeline high contrast
+(setq solarized-high-contrast-mode-line t)
+
+;; Use less bolding
+(setq solarized-use-less-bold t)
+
+;; Use more italics
+(setq solarized-use-more-italic t)
+
+;; Use less colors for indicators such as git:gutter, flycheck and similar
+(setq solarized-emphasize-indicators nil)
+
+;; Don't change size of org-mode headlines (but keep other size-changes)
+;; (setq solarized-scale-org-headlines nil)
+
+(setq x-underline-at-descent-line t)
+;;; theme
+
 ;;; add executable path
 (add-to-list 'exec-path "/usr/local/bin")
 
