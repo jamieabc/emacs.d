790d67719522e38e47f8845e6bf83c53dbe43f84
Aaron Chang <xesuschang@gmail.com>
2019-05-29 16:29:11 +0800

    commend whole line or append at end

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -584,19 +584,19 @@
 ;;; find file in project
 
 ;;; comment whole line or add tail
-;; (defun comment-whole-line-or-add-tail (&optional arg)
-;;   "Replacement for the comment-dwim command.
-;;    If no region is selected and current line is not blank and we are not at
-;;    the end of the line, then comment current line.
-;;    Replaces default behaviour of comment-dwim, when it inserts comment at the
-;;    end of the line."
-;;   (interactive "*P")
-;;   (comment-normalize-vars)
-;;   (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
-;;       (comment-or-uncomment-region (line-beginning-position) (line-end-position))
-;;     (comment-dwim arg))
-;;   )
-;; (global-set-key (kbd "M-;") 'comment-whole-line-or-add-tail)
+(defun comment-whole-line-or-add-tail (&optional arg)
+  "Replacement for the comment-dwim command.
+   If no region is selected and current line is not blank and we are not at
+   the end of the line, then comment current line.
+   Replaces default behaviour of comment-dwim, when it inserts comment at the
+   end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg))
+  )
+(global-set-key (kbd "M-;") 'comment-whole-line-or-add-tail)
 ;;; comment whole line or add tail
 
 ;;; key frequency
