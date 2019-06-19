aecfff58fbadccbeeaa3a4a941a1f24c7a735c10
Aaron Chang <xesuschang@gmail.com>
2019-06-19 15:32:33 +0800

    change font to fira code retina

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -138,7 +138,7 @@
 ;;; redmine related functions
 
 ;;; set font size
-(add-to-list 'default-frame-alist '(font . "Fira Code-14"))
+(add-to-list 'default-frame-alist '(font . "Fira Code Retina-14"))
 
 ;;; Fira code
 ;; This works when using emacs --daemon + emacsclient
